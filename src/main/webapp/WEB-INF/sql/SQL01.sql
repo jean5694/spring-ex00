6f59603edd2efc256ce10fead378d5ea82f614a8
jean5694 <jeanjean5694@gmail.com>
2021-06-21 09:52:15 +0900

    board sql

---
+++ src/main/webapp/WEB-INF/sql/SQL01.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE spr1;
+USE spr1;
+
+-- 173 page
+CREATE TABLE tbl_board (
+	bno INT PRIMARY KEY AUTO_INCREMENT, -- 자동으로 증가
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(2000) NOT NULL,
+    writer VARCHAR(50) NOT NULL,
+    regdate TIMESTAMP DEFAULT NOW(),
+    updatedate TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO tbl_board (title, content, writer)
+VALUES ('test title', 'test content', 'user00'); -- 내용 입력
+
+SELECT * FROM tbl_board; -- 조회
